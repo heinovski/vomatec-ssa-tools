3fbb1802cb70e373e2caa1d941d9c3ccb77925dc
Julian Heinovski <mail@julian-heinovski.de>
2016-10-10 18:48:47 +0200

    Read input file

--- generate-element-db-from-xml.py
+++ generate-element-db-from-xml.py
@@ -3,6 +3,16 @@
 # Copyright (c) 2016 Julian Heinovski <mail@julian-heinovski.de>
 # This script takes an element database in xml formatand and creates a corresponding txt file for SSA.
 
+import argparse
 import xml.etree.ElementTree as ET
 
-test = ET.Element("test")
+def read(filename):
+    return ET.parse(filename)
+
+def main():
+    parser = argsparse.ArgumentParser()
+    parser.add_argument("filename")
+
+    args = parser.parse_args()
+
+    root = read(args.filename)

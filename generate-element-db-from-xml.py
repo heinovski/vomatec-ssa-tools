ebb3fbb23aaed23aefcc14be74f70e67ed45612c
Julian Heinovski <mail@julian-heinovski.de>
2016-10-10 18:18:07 +0200

    Test stuff

--- generate-element-db-from-xml.py
+++ generate-element-db-from-xml.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+
+# Copyright (c) 2016 Julian Heinovski <mail@julian-heinovski.de>
+# This script takes an element database in xml formatand and creates a corresponding txt file for SSA.
+
+import xml.etree.ElementTree as ET
+
+test = ET.Element("test")
